2fd67486b8c5eea5db6f5e4d248d9f0197b1c959
AVIJIT SAMANTARAY <63417798+avijitsamantaray@users.noreply.github.com>
2024-10-04 10:56:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "hihi"          
-    storage_account_name = "tfmasterhi"                              
-    container_name       = "tfstate"                              
-    key                  = "terraform.tfstate"               
     use_oidc             = true                                  
     client_id            = "client_id_value" 
     subscription_id      = "subscription_id_value"  
