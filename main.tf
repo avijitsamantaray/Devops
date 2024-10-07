8e34467218fbcdc19e09eaccb66ced37156aec15
AVIJIT SAMANTARAY <63417798+avijitsamantaray@users.noreply.github.com>
2024-10-07 14:28:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,54 @@
-resource "github_repository" "example" {
-  name        = "example-king-testing"
-  description = "My awesome codebase"
-  visibility = "public"
 
- 
+
+variable "resource_group_name" {
+  type    = string
+  default = "hihidsdsds"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "my-storage-accountdsdsdsd"
+}
+
+variable "function_app_name" {
+  type    = string
+  default = "my-function-appdsdsdsd"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "my-app-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "EP1"
+    size = "EP1"
+  }
+}
+
+resource "azurerm_function_app" "fa" {
+  name                       = var.function_app_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
 }
