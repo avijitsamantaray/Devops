35122bfb88b82945841bc55d73777524aa01fee1
AVIJIT SAMANTARAY <63417798+avijitsamantaray@users.noreply.github.com>
2024-10-07 22:59:17 +0530

    Update snowflake_connect.py

--- src/snowflake_connect.py
+++ src/snowflake_connect.py
@@ -10,7 +10,7 @@ import sys
 
 # connection to SnowFlake
 key=os.environ['password']
-private_key = serialization.load_pem_private_key(bytes(key), password=None, backend=default_backend(encoding='PEM'))
+private_key=serialization.load_pem_private_key(bytes(key)) 
 
 
 access_key = private_key.private_bytes(
