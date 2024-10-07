a446adbc01c3b4b0999d6c6c235089cf482665b1
AVIJIT SAMANTARAY <63417798+avijitsamantaray@users.noreply.github.com>
2024-10-07 11:58:56 +0530

    Update snowflake_connect.py

--- src/snowflake_connect.py
+++ src/snowflake_connect.py
@@ -1,1 +1,36 @@
 
+import snowflake.connector
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import sys
+
+# connection to SnowFlake
+load_dotenv()
+conn=snowflake.connector.connect(
+        user=os.environ['uname'],
+        password=os.environ['password'],
+        account=os.environ['aname'],
+        database='testing',
+        schema='testing'
+
+)
+cts = conn.cursor()
+
+# Task........................................................................................
+def number1():
+    data = cts.execute("select * from day limit 5").fetchall()
+    for row in data:
+        print(row)
+
+
+
+
+if __name__ == "__main__":
+    number1()
+
+
+
+
+
+
