00ec68fbbe2aa6a17daebd151b4acb8dc15c8c74
AVIJIT SAMANTARAY <63417798+avijitsamantaray@users.noreply.github.com>
2024-10-07 12:24:27 +0530

    Update snowflake_connect.py

--- src/snowflake_connect.py
+++ src/snowflake_connect.py
@@ -1,12 +1,12 @@
 
 import snowflake.connector
-from dotenv import load_dotenv
+
 import os
 import pandas as pd
 import sys
 
 # connection to SnowFlake
-load_dotenv()
+
 conn=snowflake.connector.connect(
         user=os.environ['uname'],
         password=os.environ['password'],
