0d380b67dcefe0ce2cd2e2565894b142e7be102e
AVIJIT SAMANTARAY <63417798+avijitsamantaray@users.noreply.github.com>
2024-10-17 09:22:41 +0530

    Update set_env.py

--- actions/set_env.py
+++ actions/set_env.py
@@ -1,1 +1,7 @@
+import os
 
+def run(runner):
+    variables = runner.get_inputs('variables')
+    for line in variables.splitlines():
+        key, value = line.split('=')
+        os.environ[key] = value
